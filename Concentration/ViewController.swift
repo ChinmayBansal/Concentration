1fc32a2ccb57e1ee4e77c3fc86ec2ad47fe4ce13
Chinmay Bansal <chinmaybansalsc@outlook.com>
2022-06-01 21:53:05 -0700

    Added three more buttons, added arrays and simplified code to one function for each button and added flip label as well as flip functionality

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func touchCard(_ sender: UIButton) {
+    
+    var flipCount = 0  {
+        didSet {
+            countFlipLabel.text = "Flips: \(flipCount)"
+        }
     }
     
+    
+    @IBOutlet var cardButton: [UIButton]!
+    
+    @IBOutlet weak var countFlipLabel: UILabel!
+    
+    var emojiChoices = ["🧞‍♂️", "🥷", "🧞‍♂️", "🥷"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButton.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+
+        } else {
+            print("chose card not in array")
+        }
+        
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.red
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.systemPurple
+        }
+    }
 
 }
 
